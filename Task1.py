8adf3d8ca0b302b99f8c32c651b5f9569a14c026
Shray Rathore <49746201+meshray@users.noreply.github.com>
2025-08-26 13:12:49 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the Second number: "))
+
+print(f'Addition:{a+b}')
+print(f'Subtraction:{a-b}')
+print(f'Multiplication:{a*b}')
+print(f'Division:{a/b}')
